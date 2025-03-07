8e7cdfb213144e4e4fdcef1d177490fe5e949390
Tsoatsiafoy <tsoatsiafoy@gmail.com>
2025-03-07 16:07:16 +0300

    Mise en place projet FE dans git

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignupComponent } from './signup/signup.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'landing-page', pathMatch: 'full' },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+];
